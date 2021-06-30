84fd51f74e1cf50bf08fa666a73bfa4fb65a2213
abdipriyangga <mabdipme@gmail.com>
2021-06-30 15:33:13 +0700

    modify route auth and index

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,14 @@
 const userWorkerController = require("../controllers/userWorker");
-
+const userRecruiterController = require("../controllers/userRecruiter");
 
 const route = require("express").Router();
-const validator = require("../helpers/validationLogin");
+const validator = require("../helpers/auth/validationRegister");
+const validatorRecruiter = require("../helpers/auth/validationRegisterRecruiter");
+const validatorLogin = require("../helpers/auth/validationLogin");
 const {checkSchema} = require("express-validator");
 
 route.post("/register/worker", checkSchema(validator),  userWorkerController.registerWorker);
- 
+route.post("/register/recruiter", checkSchema(validatorRecruiter), userRecruiterController.registerRecruiter);
+route.post("/login/recruiter", checkSchema(validatorLogin), userRecruiterController.login);
+
 module.exports = route;
