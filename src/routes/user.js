f694d63db0f9c7641864c85a3ce0db63cb0ee0df
Rikirhenaldi <riki.rhenaldi24@gmail.com>
2021-07-03 02:16:05 +0700

    update profile Recruiter

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@
 
 const route = require("express").Router();
 const userController = require("../controllers/userWorker");
+const userRecruiterController = require("../controllers/userRecruiter")
 
 route.get("/details/portofolio", userController.getPortofolioByUser);
 route.get("/details/experience", userController.getExperienceByUser);
@@ -12,6 +13,9 @@ route.post("/details/editprofile/experienceWork", userController.postAddUserWork
 route.get("/details", userController.getDetailUserWorker);
 // route.get("/", userController.getUserBySkill);
 
+// User recruiter bellow this line
+
+route.put("/recruiter/details/editprofile", userRecruiterController.updateUserProfileRecruiter);
 
 
 module.exports = route;
